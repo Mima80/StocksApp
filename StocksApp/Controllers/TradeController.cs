70208d88778b2ff819df7aa713ed32366c3ec018
Mima80 <mimi451276@gmail.com>
2023-09-07 19:52:13 +0300

    Add project files.

---
+++ StocksApp/Controllers/TradeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StocksApp.Models;
+using StocksApp.ServiceContracts;
+using System.Diagnostics;
+
+namespace StocksApp.Controllers
+{
+    public class TradeController : Controller
+    {
+        private readonly IFinnhubService _finnhubService;
+        private readonly IConfiguration _configuration;
+
+        public TradeController(IFinnhubService finnhubService, IConfiguration configuration)
+        {
+            _finnhubService = finnhubService;
+            _configuration = configuration;
+        }
+        [Route("/")]
+        public IActionResult Index()
+        {
+            var companyProfileDictionary = _finnhubService.GetCompanyProfile(_configuration["TradingOptions:DefaultStockSymbol"]).Result;
+            var stockQuoteDictionary = _finnhubService.GetStockPriceQuote(_configuration["TradingOptions:DefaultStockSymbol"]).Result;
+            var stockTrade = new StockTrade()
+            {
+                StockSymbol = companyProfileDictionary["ticker"].ToString(),
+                StockName = companyProfileDictionary["name"].ToString(),
+                Price = double.Parse(stockQuoteDictionary["c"].ToString().Replace('.', ','))
+            };
+            return View(stockTrade);
+        }
+    }
+}
---
+++ StocksApp/Services/FinnhubService.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json;
+using StocksApp.ServiceContracts;
+namespace StocksApp.Services
+{
+    public class FinnhubService : IFinnhubService
+    {
+        //GetCompanyProfile: https://finnhub.io/api/v1/stock/profile2?symbol={symbol}&token={token}
+        //GetStockPriceQuote: https://finnhub.io/api/v1/quote?symbol={symbol}&token={token}
+        private readonly IConfiguration _configuration;
+        private readonly IHttpClientFactory _httpClientFactory;
+        public FinnhubService(IConfiguration configuration, IHttpClientFactory httpClientFactory)
+        {
+            _configuration = configuration;
+            _httpClientFactory = httpClientFactory;
+        }
+        public Task<Dictionary<string, object>?> GetCompanyProfile(string stockSymbol)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://finnhub.io/api/v1/stock/profile2?symbol={stockSymbol}&token={_configuration["FinnhubToken"]}")
+            };
+            var httpResponseMessage = httpClient.Send(httpRequestMessage);
+            var responseDictionary = httpResponseMessage.Content.ReadFromJsonAsync<Dictionary<string, object>>();
+            if (responseDictionary.Result == null)
+                throw new InvalidOperationException("No response from server");
+            if (responseDictionary.Result.ContainsKey("error"))
+                throw new InvalidOperationException(Convert.ToString(responseDictionary.Result["error"]));
+            return responseDictionary;
+        }
+        public Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://finnhub.io/api/v1/quote?symbol={stockSymbol}&token={_configuration["FinnhubToken"]}")
+            };
+            var httpResponseMessage = httpClient.Send(httpRequestMessage);
+            var responseDictionary = httpResponseMessage.Content.ReadFromJsonAsync<Dictionary<string, object>>();
+            if (responseDictionary.Result == null)
+                throw new InvalidOperationException("No response from server");
+            if (responseDictionary.Result.ContainsKey("error"))
+                throw new InvalidOperationException(Convert.ToString(responseDictionary.Result["error"]));
+            return responseDictionary;
+        }
+    }
+}
